2c5bf7d36f259cf25117bfe3461369e1c492f090
luanalobato1 <luanalobato1>
2023-05-01 20:39:04 -0300

    se creo la pagina de home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
 import Card from '../../components/Card/Card'
+import './home.css'
 
 const Home = () => {
   return (
