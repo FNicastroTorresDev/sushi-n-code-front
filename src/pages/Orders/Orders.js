209b6b9bb5a61cb624f56f616ece39a363240f88
Daniel Teseira <daniel.teseira@alu.frt.utn.edu.ar>
2023-05-02 20:45:34 -0300

    se agergo redireccionado en orders.js para localhost

--- src/pages/Orders/Orders.js
+++ src/pages/Orders/Orders.js
@@ -1,41 +1,45 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import './orders.css'
+import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Footer/Footer'
 
 const Producto = () => {
   const params = useParams()
   const [product, setProduct] = useState(null)
-  const [cantidad, setCantidad] = useState('1')
+  const [ setCantidad] = useState('1')
 
-  const fetchProduct = async (id) => {
-    const response = await fetch(`...`)
+  const fetchProduct = async (_id) => {
+    const response = await fetch(`http://localhost:8000/api/menues/64518014224b0f59582c50a6`)
     const data = await response.json()
     
     setProduct(data)
   }
-
+  // console.log(product.menu)
   useEffect(() => {
-    fetchProduct(params.id)
-  }, [params.id])
+    fetchProduct(params._id)
+  }, [params._id])
 
   if (!product) return <h3 className='text-center mt-3'>Cargando producto...</h3>
 
   return (
-    <section className='product-detail-container container p-4 mt-5'>
-      <p><h3 className='d-flex justify-content-center'> {...} </h3></p>
+    <>
+       <Navbar />
+      <section className='product-detail-container container p-4 mt-5'>
+      <p><h3 className='d-flex justify-content-center'> {product.menu.name} </h3></p>
 
       <div className='d-flex row contenido'>
-        <img src={...} className='col-4 image' alt={...}/>
+        <img src={product.menu.imgUrl} className='col-4 image' alt={product.menu.name}/>
 
         <div className='d-flex flex-column col-8 info'>
-          <p> {...} </p>
-          {/* <span class="badge bg-secondary col-2"> {product.category} </span> */}
-          <h4 className='mt-3'>${...}</h4>
+          <p> {product.menu.details} </p>
+          <span class="badge bg-secondary col-2"> {product.menu.category} </span>
+          <h4 className='mt-3'>${product.menu.price}</h4>
                   
           <div className='d-flex align-items-center mx-5  justify-content-end comprar'>
-            {/* <p className='mx-3 my-0'> Hay {product.rating.count} 
-                disponible!! 
-            </p> */}
+            <p className='mx-3 my-0'> Hay {product.menu.state} 
+                !! 
+            </p>
              <h5 className='my-0'> Cantidad 
             </h5>
             <select className="form-select mx-3" aria-label="Default select example" onChange={(event) => setCantidad(event.target.value)}>              
@@ -57,6 +61,9 @@ const Producto = () => {
         </div>
       </div>
     </section>
+    <Footer />
+    </>
+    
   )
 }
 
